90d26bd5aac395ed8b1f0637333ca6a580036f31
serena garuti <sere.garuti@gmail.com>
2019-03-08 17:55:56 +0100

    Done kind of

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+console.log('Hello Wr')
 
 import 'bootstrap';
+
