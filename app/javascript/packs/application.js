5af067b823e317916c0fcaa5b3ea33102b00174f
serena garuti <sere.garuti@gmail.com>
2019-03-10 17:40:58 +0100

    more css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,19 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello Wr')
+console.log('Hello Word')
 
 import 'bootstrap';
 
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    const link = document.querySelector('#delete-link');
+    link.click();
+  }
+});
